29acd9783dc2f0c7c8c06a746a37537ef5a636d3
Sourav Sadhukhan <ssadhukhan990@gmail.com>
2025-06-11 02:13:45 +0530

    Add dotenv configuration to firebase.ts for environment variable management

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getDatabase, Database} from "firebase/database";
